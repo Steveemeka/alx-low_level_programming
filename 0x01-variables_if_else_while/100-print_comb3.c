d98a64ed1b4e50205d5db594585c4a8ba70008e8
Steveemeka <steveemeka91@gmail.com>
2022-12-10 13:54:06 -0800

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -26,24 +26,25 @@ int main(void)
 	 { 
 		 c = 0;
 		 while (c < 10);
-		  { 
+		 {
 			  if (d != c && d < c)
-			 { 
+		       	  {
 				 putchar('0' + d);
 				 putchar('0' + c);
 
 				 if (c + d != 17)
-				{
+				 {
 					putchar(',');
 					putchar(' ');
-				}
-			}
+				 }
+			  }
 
 			  c++;
-		}
+		 }
 
 		  d++;
-	}
+	 }
 	 putchar('\n');
 	 return (0);
-}
+
+ }
