320b5a4cc58451d7efe641cbffe21d31a13b9ffa
Steveemeka <steveemeka91@gmail.com>
2023-01-02 02:50:48 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,9 +10,9 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 	
-	for (j = 0; *(s + j); j++);
+	for (j = 0; *(s + j); j++)
 	{
-		for (i = 0; *(accept + i); i++);
+		for (i = 0; *(accept + i); i++)
 		{
 			if (*(s + j) == *(accept + i))
 				break;
