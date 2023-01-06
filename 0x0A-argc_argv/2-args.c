5fa75be7ab37238f211edc3b42357566c547a236
Steveemeka <steveemeka91@gmail.com>
2023-01-06 07:10:11 -0800

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,12 @@
  *
  * @argc: number of arguments
  * @argv: arrays of arguments
- *
  * Return: Always (0) success.
  */
 int main(int argc, char *argv[])
 {
 	int n;
-	
+
 	for (n = 0; n < argc; n++)
 	{
 		printf("%s\n", argv[n]);
