1e66b2cb9a94aa162dc18bac3a34bc971984ab84
Steveemeka <steveemeka91@gmail.com>
2023-01-12 09:41:20 -0800

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,6 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		nptr = malloc(new_size);
 		if (nptr == NULL)
 			return (NULL);
+	}
 	if (new_size > old_size && (ptr != NULL))
 	{
 		nptr = malloc(new_size);
